16cf1844aa8a4cb0a067331d669a0adffc5e1a6c
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-28 17:58:17 +0900

    강의내용(앱 분석)

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -14,11 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberLabel: UILabel!
     
     // 컴퓨터는 랜덤숫자 선택하기
+    // 클래스 내부에 속성 comNumber 만듦
     var comNumber = Int.random(in: 1...10)
     // var myNumber: Int = 1
     // 에러발생 가능성 예방을 위해 0대신 1을 넣어주는게 낫다
     
     override func viewDidLoad() {
+        // 상위 메서드 호출
         super.viewDidLoad()
         // 메인레이블에 선택하세요 라고 표시
         mainLabel.text = "선택하세용"
@@ -29,6 +31,7 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
     // 버튼의 숫자를 가져온다
+    // 가드문사용
     // guard let numString = sender.currentTitle else { return }
         let numString = sender.currentTitle!
     // 숫자 레이블이 숫자에 따라 변하도록 만든다
@@ -50,7 +53,9 @@ class ViewController: UIViewController {
          */
         guard let numString = numberLabel.text else { return }
         guard let myNumber = Int(numString) else { return }
-
+        // 이렇게 가드문도 한번에 두개의 조건 사용가능 
+        // guard let numString = numberLabel.text,
+        // let myNumber = Int(numString) else { return }
         if comNumber < myNumber {
             mainLabel.text = "Down"
         } else if comNumber > myNumber {
